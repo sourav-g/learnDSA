8201329a08dc92c4bbe8bbe33b01daf0c1611b62
Sourav Ghosh <gsourav.official@gmail.com>
2022-02-07 23:05:35 +0530

    sum pair present

--- hashing/pair_in_array_with_given_sum(unsorted).js
+++ hashing/pair_in_array_with_given_sum(unsorted).js
@@ -1,10 +1,24 @@
-let arr = [3,2,8,15,-8];   //? [n*(n-1)]/2 possible pairs
+let arr = [3,2,8,15,-8];   //? [n*(n-1)]/2 possible pairs //n=5
 const sum = 17;
 
+//*Approch 1 : Using hashing
+//TC = O(n)
+//SC = O(n)
+console.log(isPairPresent(arr));
+function isPairPresent(arr){
+    let set = new Set();
+    for(let i=0; i<arr.length; i++){
+        let diff = sum - arr[i];
+        if(set.has(diff)){
+            return true;
+        }else{
+            set.add(arr[i]);
+        }
+    }
+    return false;
+}
 
-
-
-
-
-// TODO : also solving by sorting the array and using 2 pointer approach
+//* TODO 
+// Approach 2 : Sort + 2 pointer approach 
 // TC = O(nlogn) {logn for sort + n for linear search for the pair}
+// TC = O(n) for sorted array
