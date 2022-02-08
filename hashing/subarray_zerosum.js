260b83ce4f0fb9d53a57a26f3ebf55936fac8c8f
Sourav Ghosh <gsourav.official@gmail.com>
2022-02-09 01:26:20 +0530

    zero sum subarray

---
+++ hashing/subarray_zerosum.js
@@ -0,0 +1,24 @@
+let arr = [1,4,13,-3,-10,5];  
+
+//?Sub Array is contiguos, else its a subsequence
+//*No of subarrays => (n+1)+ n(n-1)/2 => 22 (for n=6) 
+//*Prefix sum + hashing
+// TC = O(n)
+// SC = O(n)
+
+console.log(isZeroSumSubArray(arr));
+function isZeroSumSubArray(arr){
+    let prefixSum = 0;
+    let set = new Set();
+    for(let i=0; i<arr.length; i++){
+        prefixSum += arr[i];
+        if(set.has(prefixSum)){
+            return true;
+        }else{
+            if(prefixSum==0) return true;
+            else
+            set.add(prefixSum);
+        }
+    }
+    return false;
+}
