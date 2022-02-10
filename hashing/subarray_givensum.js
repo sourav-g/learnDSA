691ed9da5679ee447b3a64c3132ac4916b7c1368
Sourav Ghosh <gsourav.official@gmail.com>
2022-02-11 01:27:55 +0530

    subarray_givensum

---
+++ hashing/subarray_givensum.js
@@ -0,0 +1,28 @@
+let arr = [1,7,13,2,7,5];
+let sum = 21;
+
+//?Sub Array is contiguos, else its a subsequence
+//*No of subarrays => (n+1)+ n(n-1)/2 => 22 (for n=6) 
+//*Prefix sum + hashing
+// TC = O(n)
+// SC = O(n)
+
+console.log(isGivenSumSubArray(arr));
+function isGivenSumSubArray(arr){
+    let prefixSum = 0;
+    let set = new Set();
+    for(let i=0; i<arr.length; i++){
+        prefixSum += arr[i];
+        if(prefixSum==sum) return true; //of subarray starts from 0 case
+        set.add(prefixSum);
+    }
+    
+    //prefixsum => 1,5,18,20,27,32
+
+    for(let value of set){
+        if(set.has(value+sum)){ //O(1) considering good hashing fn.
+            return true;
+        }
+    }
+    return false;
+}    
--- hashing/subarray_zerosum.js
+++ hashing/subarray_zerosum.js
@@ -1,4 +1,4 @@
-let arr = [1,4,13,-3,-10,5];  
+let arr = [1,4,-4,-3,-10,5];  
 
 //?Sub Array is contiguos, else its a subsequence
 //*No of subarrays => (n+1)+ n(n-1)/2 => 22 (for n=6) 
@@ -11,6 +11,7 @@ function isZeroSumSubArray(arr){
     let prefixSum = 0;
     let set = new Set();
     for(let i=0; i<arr.length; i++){
+        //prefixSum = 1,5,18,15,5,10  => 5 repeats
         prefixSum += arr[i];
         if(set.has(prefixSum)){
             return true;
